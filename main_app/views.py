8acebfbf5964dab766fe351c3cbc94b27fbfde4f
Christian Watson <chris23w@gmail.com>
2019-06-10 13:46:39 -0700

    initial commit

---
+++ main_app/views.py
@@ -0,0 +1,18 @@
+from django.shortcuts import render
+from django.http import HttpResponse
+from .models import Dog
+
+# Create your views here.
+def home(request):
+  return HttpResponse('<h1>Hello my dog</h1>')
+
+def about(request):
+  return render(request, 'about.html')
+
+def dogs_index(request):
+  dogs = Dog.objects.all()
+  return render(request, 'dogs/index.html', {'dogs': dogs})
+
+def dogs_detail(request, dog_id):
+  dog = Dog.objects.get(id=dog_id)
+  return render(request, 'dogs/detail.html', {'dog': dog})
