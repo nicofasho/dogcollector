1e52f42e20d34849d32067714c248fc584f6342e
Christian Watson <chris23w@gmail.com>
2019-06-11 13:45:06 -0700

    feeding adding on detail page

--- main_app/models.py
+++ main_app/models.py
@@ -31,3 +31,6 @@ class Feeding(models.Model):
   dog = models.ForeignKey(Dog, on_delete=models.CASCADE)
   def __str__(self):
     return f"{self.get_meal_display()} on {self.date}"
+
+  class Meta:
+    ordering = ['-date']
--- main_app/views.py
+++ main_app/views.py
@@ -1,7 +1,8 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
 from django.http import HttpResponse
 from .models import Dog
 from django.views.generic.edit import CreateView, UpdateView, DeleteView
+from .forms import FeedingForm
 
 # Create your views here.
 def home(request):
@@ -16,7 +17,16 @@ def dogs_index(request):
 
 def dogs_detail(request, dog_id):
   dog = Dog.objects.get(id=dog_id)
-  return render(request, 'dogs/detail.html', {'dog': dog})
+  feeding_form = FeedingForm()
+  return render(request, 'dogs/detail.html', {'dog': dog, 'feeding_form': feeding_form})
+
+def add_feeding(request, dog_id):
+  form = FeedingForm(request.POST)
+  if form.is_valid():
+    new_feeding = form.save(commit=False)
+    new_feeding.dog_id = dog_id
+    new_feeding.save()
+  return redirect('detail', dog_id=dog_id)
 
 class DogCreate(CreateView):
   model = Dog
