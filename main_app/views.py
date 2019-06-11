04ebca456e66cd1a94f3e0bfec4102b9ad3fbec1
Christian Watson <chris23w@gmail.com>
2019-06-11 10:06:12 -0700

    CRUD implemented for dogs

--- main_app/views.py
+++ main_app/views.py
@@ -1,6 +1,7 @@
 from django.shortcuts import render
 from django.http import HttpResponse
 from .models import Dog
+from django.views.generic.edit import CreateView, UpdateView, DeleteView
 
 # Create your views here.
 def home(request):
@@ -16,3 +17,16 @@ def dogs_index(request):
 def dogs_detail(request, dog_id):
   dog = Dog.objects.get(id=dog_id)
   return render(request, 'dogs/detail.html', {'dog': dog})
+
+class DogCreate(CreateView):
+  model = Dog
+  fields = '__all__'
+  success_url = '/dogs/'
+
+class DogUpdate(UpdateView):
+  model = Dog
+  fields = ['breed', 'description', 'age'] 
+
+class DogDelete(DeleteView):
+  model = Dog
+  success_url = '/dogs/'
