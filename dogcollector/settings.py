047f9b40be471d317885bc9ef28d749137dc3077
Christian Watson <chris23w@gmail.com>
2019-06-13 12:01:13 -0700

    authentication and authorization implemented

--- dogcollector/settings.py
+++ dogcollector/settings.py
@@ -123,6 +123,8 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/2.2/howto/static-files/
 
 STATIC_URL = '/static/'
+LOGIN_REDIRECT_URL = '/dogs/'
+LOGOUT_REDIRECT_URL = '/'
 STATIC_ROOT = 'main_app/static/'
 
 STATICFILES_FINDERS = [
--- main_app/views.py
+++ main_app/views.py
@@ -6,20 +6,27 @@ from .forms import FeedingForm
 import uuid
 import boto3
 from .models import Dog, Toy, Photo
+from django.contrib.auth import login
+from django.contrib.auth.forms import UserCreationForm
+from django.contrib.auth.decorators import login_required
+from django.contrib.auth.mixins import LoginRequiredMixin
+
 S3_BASE_URL = 'https://s3-us-west-1.amazonaws.com/'
 BUCKET = 'dogcollector'
 
 # Create your views here.
 def home(request):
-  return HttpResponse('<h1>Hello my dog</h1>')
+  return render(request, 'home.html')
 
 def about(request):
   return render(request, 'about.html')
 
+@login_required
 def dogs_index(request):
-  dogs = Dog.objects.all()
+  dogs = Dog.objects.filter(user=request.user)
   return render(request, 'dogs/index.html', {'dogs': dogs})
 
+@login_required
 def dogs_detail(request, dog_id):
   dog = Dog.objects.get(id=dog_id)
   toys_dog_doesnt_have = Toy.objects.exclude(id__in = dog.toys.all().values_list('id'))
@@ -31,6 +38,7 @@ def dogs_detail(request, dog_id):
     })
 
 
+@login_required
 def add_feeding(request, dog_id):
   form = FeedingForm(request.POST)
   if form.is_valid():
@@ -44,36 +52,49 @@ class DogCreate(CreateView):
   fields = [ 'name', 'breed', 'description', 'age' ]
   success_url = '/dogs/'
 
-class DogUpdate(UpdateView):
+  # this inherited method is called when a 
+  # valid dog form is being submitted
+  def form_valid(self, form):
+    form.instance.user = self.request.user
+    return super().form_valid(form)
+
+class DogUpdate(LoginRequiredMixin, UpdateView):
   model = Dog
   fields = ['breed', 'description', 'age'] 
 
-class DogDelete(DeleteView):
+class DogDelete(LoginRequiredMixin, DeleteView):
   model = Dog
   success_url = '/dogs/'
 
-class ToyList(ListView):
+class ToyList(LoginRequiredMixin, ListView):
   model = Toy
 
-class ToyDetail(DetailView):
+class ToyDetail(LoginRequiredMixin, DetailView):
   model = Toy
 
-class ToyCreate(CreateView):
+class ToyCreate(LoginRequiredMixin, CreateView):
   model = Toy
   fields = '__all__'
 
-class ToyUpdate(UpdateView):
+class ToyUpdate(LoginRequiredMixin, UpdateView):
   model = Toy
   fields = ['name', 'color']
 
-class ToyDelete(DeleteView):
+class ToyDelete(LoginRequiredMixin, DeleteView):
   model = Toy
   success_url = '/toys/'
 
+@login_required
 def assoc_toy(request, dog_id, toy_id):
   Dog.objects.get(id=dog_id).toys.add(toy_id)
   return redirect('detail', dog_id=dog_id)
 
+@login_required
+def unassoc_toy(request, dog_id, toy_id):
+  Dog.objects.get(id=dog_id).toys.remove(toy_id)
+  return redirect('detail', dog_id=dog_id)
+
+@login_required
 def add_photo(request, dog_id):
 	# photo-file was the "name" attribute on the <input type="file">
     photo_file = request.FILES.get('photo-file', None)
@@ -92,3 +113,17 @@ def add_photo(request, dog_id):
         except:
             print('An error occurred uploading file to S3')
     return redirect('detail', dog_id=dog_id)
+
+def signup(request):
+  error_message = ''
+  if request.method == 'POST':
+    form = UserCreationForm(request.POST)
+    if form.is_valid():
+      user = form.save()
+      login(request, user)
+      return redirect('index')
+    else:
+      error_message = 'Invalid sign up - try again'
+  form = UserCreationForm()
+  context = {'form': form, 'error_message': error_message}
+  return render(request, 'registration/signup.html', context)
